513b837dad3e4cb28a139e903817174d94fdad18
lucas-couto <lslopes1903@hotmail.com>
2022-07-26 12:04:05 -0300

    :sparkles: Adding initial configurations

---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    border: none;
+    text-decoration: none;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%; /* 10px */
+  }
+`;

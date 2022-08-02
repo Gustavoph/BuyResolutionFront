2e7d87eed1c5437c36882a30a76f48205fbf308a
Lucas Couto <lslopes1903@hotmail.com>
2022-08-02 10:08:10 -0300

    :art: Adding global container and components container

--- src/routes/styles.ts
+++ src/routes/styles.ts
@@ -4,12 +4,7 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`
-
-export const Content = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  max-width: 112rem;
-  margin: 0 auto;
+  width: 100%;
+  min-height: 100vh;
+  background-color: ${({ theme }) => theme.colors.primary};
 `
